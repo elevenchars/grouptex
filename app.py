905322b01ccdede104df34ea0484abed61164fae
willi <will.ingarfield@gmail.com>
2018-12-23 13:07:39 -0600

    receive webhook messages from test group

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+import pprint # debugging json requests
+import json
 
-@app.route("/")
+app = Flask(__name__)
+pp = pprint.PrettyPrinter(indent=4)
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "Hello, World!"
+    if request.method == "GET":
+        return "<h1>You're not supposed to see this, silly!</h1>"
+    elif request.method == "POST":
+        message = request.get_json()
+        print("[{}] {}: {}".format(message["sender_id"], message["name"], message["text"]))
+        return ""
