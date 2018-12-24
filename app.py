43f020db82fde36383169b5ace56ee7e21d465f2
willi <will.ingarfield@gmail.com>
2018-12-23 20:03:26 -0600

    cleaning up

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import logging
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+def index():
     if request.method == "GET":
-        return "<h1>You're not supposed to see this, silly!</h1>"
+        return "<h1>GroupTeX webhook</h1>"
     elif request.method == "POST":
         message = request.get_json()
         app.logger.info("[{}] {}: {}".format(message["sender_id"], message["name"], message["text"]))
